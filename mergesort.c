f736029a023b15ab1c396dc24215070bb9810453
trung.vuong <demonkid98@gmail.com>
2017-03-10 11:07:10 +0100

    mergesort: seq 1st try

--- mergesort.c
+++ mergesort.c
@@ -19,21 +19,21 @@ static array_int X ;
 void init_array (array_int T)
 {
   register int i ;
-
+  
   for (i = 0 ; i < N ; i++)
-    {
-      T [i] = N - i ;
-    }
+  {
+    T [i] = N - i ;
+  }
 }
 
 void print_array (array_int T)
 {
   register int i ;
-
+  
   for (i = 0 ; i < N ; i++)
-    {
-      printf ("%d ", T[i]) ;
-    }
+  {
+    printf ("%d ", T[i]) ;
+  }
   printf ("\n") ;
 }
 
@@ -42,10 +42,10 @@ int is_sorted (array_int T)
   register int i ;
   
   for (i = 1 ; i < N ; i++)
-    {
-      if (T[i-1] > T [i])
-	return 0 ;
-    }
+  {
+    if (T[i-1] > T [i])
+    return 0 ;
+  }
   return 1 ;
 }
 
@@ -53,26 +53,59 @@ long long unsigned int average (long long unsigned int *exps)
 {
   unsigned int i ;
   long long unsigned int s = 0 ;
-
+  
   for (i = 2; i < (NBEXPERIMENTS-2); i++)
-    {
-      s = s + exps [i] ;
-    }
-
+  {
+    s = s + exps [i] ;
+  }
+  
   return s / (NBEXPERIMENTS-2) ;
 }
 
 
-
 void merge_sort (int *T, const int size)
 {
-    /* TODO: sequential version of the merge sort algorithm */
+  /* TODO: sequential version of the merge sort algorithm */
+  if (size > 1) {
+    merge_sort(T, size / 2);
+    merge_sort(&T[size / 2], size / 2);
+  }
+
+  register unsigned int i = 0;
+  register unsigned int j = size;
+  register unsigned int k = 0;
+  int *X = malloc(size * sizeof(int) * 2);
+
+  while (i < size && j <  2 * size) {
+    if (T[i] < T[j]) {
+      X[k] = T[i];
+      i++;
+    } else {
+      X[k] = T[j];
+      j++;
+    }
+    k++;
+  }
+
+  while (i < size) {
+    X[k] = T[i];
+    i++;
+    k++;
+  }
+  while (j < 2 * size) {
+    X[k] = T[j];
+    j++;
+    k++;
+  }
+
+  memcpy(T, X, size * sizeof(int) * 2);
+  free(X);
 }
 
 
 void parallel_merge_sort (int *T, const int size)
 {
-    /* TODO: sequential version of the merge sort algorithm */
+  /* TODO: sequential version of the merge sort algorithm */
 }
 
 
@@ -81,70 +114,70 @@ int main (int argc, char **argv)
   unsigned long long int start, end, residu ;
   unsigned long long int av ;
   unsigned int exp ;
-
-    if (argc != 2)
-    {
-      fprintf (stderr, "mergesort N \n") ;
-      exit (-1) ;
-    }
-
+  
+  if (argc != 2)
+  {
+    fprintf (stderr, "mergesort N \n") ;
+    exit (-1) ;
+  }
+  
   N = 1 << (atoi(argv[1])) ;
   X = (int *) malloc (N * sizeof(int)) ;
-
+  
   printf("--> Sorting an array of size %u\n",N);
   
   start = _rdtsc () ;
   end   = _rdtsc () ;
   residu = end - start ; 
-
+  
   // print_array (X) ;
-
+  
   printf("sequential sorting ...\n");
-
-
-    for (exp = 0 ; exp < NBEXPERIMENTS; exp++)
+  
+  
+  for (exp = 0 ; exp < NBEXPERIMENTS; exp++)
+  {
+    init_array (X) ;
+    
+    start = _rdtsc () ;
+    
+    merge_sort (X, N) ;
+    
+    end = _rdtsc () ;
+    experiments [exp] = end - start ;
+    
+    if (! is_sorted (X))
     {
-      init_array (X) ;
-      
-      start = _rdtsc () ;
-
-               merge_sort (X, N) ;
-     
-      end = _rdtsc () ;
-      experiments [exp] = end - start ;
-      
-      if (! is_sorted (X))
-	{
-            fprintf(stderr, "ERROR: the array is not properly sorted\n") ;
-            exit (-1) ;
-	}      
-    }
+      fprintf(stderr, "ERROR: the array is not properly sorted\n") ;
+      exit (-1) ;
+    }      
+  }
   
   av = average (experiments) ;  
   printf ("\n merge sort serial\t\t %Ld cycles\n\n", av-residu) ;
-
+  
   printf("parallel sorting ...\n");
   
   for (exp = 0 ; exp < NBEXPERIMENTS; exp++)
+  {
+    init_array (X) ;
+    
+    start = _rdtsc () ;
+    
+    parallel_merge_sort (X, N) ;
+    
+    end = _rdtsc () ;
+    experiments [exp] = end - start ;
+    
+    if (! is_sorted (X))
     {
-      init_array (X) ;
-      
-      start = _rdtsc () ;
-
-           parallel_merge_sort (X, N) ;
-     
-      end = _rdtsc () ;
-      experiments [exp] = end - start ;
-
-      if (! is_sorted (X))
-	{
-            fprintf(stderr, "ERROR: the array is not properly sorted\n") ;
-            exit (-1) ;
-	}      
-    }
+      fprintf(stderr, "ERROR: the array is not properly sorted\n") ;
+      exit (-1) ;
+    }      
+  }
   
   av = average (experiments) ;
   printf ("\n merge sort parallel with tasks\t %Ld cycles\n\n", av-residu) ;
-
+  
   
 }
